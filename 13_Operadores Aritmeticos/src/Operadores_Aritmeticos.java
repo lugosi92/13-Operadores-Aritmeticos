98a6b62b94287a62babda53fbeb1858b8bbc2094
khaled___beno <khaled___beno@localhost>
2023-12-26 19:22:51 +0100

    Operadores Aritmeticos

---
+++ 13_Operadores Aritmeticos/src/Operadores_Aritmeticos.java
@@ -0,0 +1,33 @@
+
+public class Operadores_Aritmeticos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*
+		 * Suma +
+		 * Resta - 
+		 * Multiplicacion *
+		 * Division
+		 * Modulo % El recibo de las divisones
+		 */
+		
+		int a, b;
+		int resultado =0;
+		a =5;
+		b= 10;
+		
+		
+		//Operacion
+		resultado = 43 / 3;
+		
+		
+		
+		
+		
+		System.out.println(resultado);
+	
+	}
+
+}
